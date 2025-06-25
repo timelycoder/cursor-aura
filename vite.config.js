558496c36a54d78b47efbab240fd99ba99f02a1d
timelycoder <sms.sumon@gmail.com>
2025-06-25 18:59:51 +0600

    build in docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/cursor-aura/", //  এটা  GitHub রিপোজিটরির নাম
+  build: {
+    outDir: "docs", //  output যাবে /docs ফোল্ডারে
+  },
+});
